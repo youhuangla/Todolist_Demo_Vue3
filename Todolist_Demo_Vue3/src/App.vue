29a31de8d38deae1b34d0015781870eb9d046fc1
youhuangla <youhuangla123@163.com>
2023-08-01 16:25:13 +0800

    Create a Vue component with an input field that displays content above the input box based on user input

--- Todolist_Demo_Vue3/src/App.vue
+++ Todolist_Demo_Vue3/src/App.vue
@@ -1,30 +1,17 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="app">
+    <h2>{{ title }}</h2>
+    <input type="text" v-model="title">
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<script setup>
+import { ref } from 'vue';
+
+// 定义一个响应式数据
+const title = ref('');
+</script>
+
+<style>
+/* 在这里定义组件的样式 */
 </style>
