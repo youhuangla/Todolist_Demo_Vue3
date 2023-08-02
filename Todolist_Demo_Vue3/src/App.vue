20ce6e8930a91a54fc03abbe6c174f43ab110a9b
youhuangla <youhuangla123@163.com>
2023-08-02 11:42:32 +0800

    Add a properties 'done' to object literal, create a 'done' style to show finished work

--- Todolist_Demo_Vue3/src/App.vue
+++ Todolist_Demo_Vue3/src/App.vue
@@ -4,8 +4,9 @@
     <input type="text" v-model="title" @keydown.enter="addTodo">
   </div>
   <ul>
-    <li v-for="todo in todos">
-      {{ todo.title }}
+    <li v-for="todo in todos" :key="todo.title">
+      <input type="checkbox" v-model="todo.done">
+      <span :class="{ done: todo.done }">{{ todo.title }}</span>
     </li>
   </ul>
 </template>
@@ -16,15 +17,20 @@ import { ref } from 'vue';
 // 定义一个响应式数据
 const title = ref('');
 const todos = ref([
-  {title: 'Eat'}, 
-  {title: 'Sleep'}, 
-  {title: 'Code'}]);
+  { title: 'Eat', done: false },
+  { title: 'Sleep', done: false },
+  { title: 'Code', done: false }
+]);
 const addTodo = () => {
-  todos.value.push({title: title.value});
+  todos.value.push({ title: title.value, done: true });
   title.value = '';
 };
 </script>
 
 <style>
 /* 在这里定义组件的样式 */
+.done {
+  color: gray;
+  text-decoration: line-through;
+}
 </style>
